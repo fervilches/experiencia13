f38af48af25f15b03bbd901f4199237896f0663a
Fernanda Vilches <f.vilchesc@gmail.com>
2017-08-28 19:33:22 -0300

    ejercicio 1 y 2 ok

---
+++ ejercicio2.rb
@@ -0,0 +1,20 @@
+productos = {'bebida' => 850, 'chocolate' => 1200,
+'galletas' => 900, 'leche' => 750}
+
+productos.each { |valor, producto| puts producto }
+
+productos['cereal'] = 2200
+
+puts productos
+
+productos['bebida'] = 2000
+
+puts productos
+
+nuevavariable = productos.to_a
+
+puts nuevavariable
+
+productos.delete('galletas')
+
+puts productos
