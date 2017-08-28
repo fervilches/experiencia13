e4dd8eef4e373eafca1f64ee6771b0079265159f
Fernanda Vilches <f.vilchesc@gmail.com>
2017-08-28 19:47:32 -0300

    2 ok y 3 a la mitad

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,7 +1,7 @@
 productos = {'bebida' => 850, 'chocolate' => 1200,
 'galletas' => 900, 'leche' => 750}
 
-productos.each { |valor, producto| puts producto }
+productos.each { |producto, valor| puts producto }
 
 productos['cereal'] = 2200
 
