6a9d8850e00096b47117f72b8d6cab6431b64658
Fernanda Vilches <f.vilchesc@gmail.com>
2017-08-28 20:27:23 -0300

    4 ok

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,5 +1,14 @@
-personas = [["Carolina", 32] ["Alejandro", "Maria Jesús", "Valentín"]
+personas = [["Carolina", 32], ["Alejandro", 28], ["Maria Jesús", 41], ["Valentín", 19]]
+
+personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
 edades = [32, 28, 41, 19]
 
+personas_hash = Hash[personas.zip edades]
 
-print personas.to_h
+#crear un método que reciba un nombre y muestre la edades
+
+def wierd_method(hash, key)
+  hash[key]
+end
+
+puts wierd_method(personas_hash, 'Carolina')
